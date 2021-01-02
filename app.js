d6608944d5d953266719a62d4f1f8c2b1cefba18
Saji <sewangco@gmail.com>
2021-01-02 19:03:52 +0100

    integrating async/await

--- app.js
+++ app.js
@@ -4,46 +4,39 @@
 
 const puzzleEl = document.querySelector('#puzzle')
 const guessesEl = document.querySelector('#guesses')
-const game1 = new Hangman('car parts', 2)
+// let game1 = new Hangman('car parts', 2)
+let game1
 
-puzzleEl.textContent = game1.puzzle
-guessesEl.textContent= game1.statusMessage
-
-
-// const game2 = new Hangman('New Jersey', 4)
-// game2.makeGuess('w')
-// console.log(game2.getPuzzle())
 
 window.addEventListener('keypress',(e) => {
     const guess= String.fromCharCode(e.charCode)
     game1.makeGuess(guess)
+    render()
+})
+
+const render= ()=> {
     puzzleEl.textContent= game1.puzzle
     guessesEl.textContent =game1.statusMessage
+}
+const startGame = async() => {
+    const puzzle = await getPuzzle('2')
+    game1= new Hangman(puzzle, 5)
+    render()
+}
+
+
+getPuzzle('2').then((puzzle) => {
+    console.log(puzzle)
+}).catch((err) =>{
+    console.log(`Error: ${err}`)
 })
 
-// //callback function
-// getPuzzle('2').then((puzzle) => {
-//     console.log(puzzle)
-// }, (err) =>{
-//     console.log(`Error: ${err}`)
-// })
-
-// // making an http request 
-
-// getCountry("US").then((country) => {
-//     console.log(`Country name: ${country.name}`)
-// }, (err) => {
-//     console.log(`Error: ${err}`)
-// })
-
-fetch('http://puzzle.mead.io/puzzle', {}).then((response) => {
-    if (response.status === 200){
-        return response.json()
-    } else {
-        throw new Error('Unable to fetch the puzzle')
-    }
-}).then((data)=> {
-    console.log(data.puzzle);
-}).catch((error) => {
-    console.log(error);
+document.querySelector('#reset').addEventListener('click', ()=> {
+    startGame()
 })
+
+startGame()
+
+
+
+
--- requests.js
+++ requests.js
@@ -1,33 +1,40 @@
-const getPuzzle = (wordCount) => new Promise((resolve,reject) => {
-    const request= new XMLHttpRequest()
+const getPuzzle = async (wordCount) => {
+    const response = await fetch(`http://puzzle.mead.io/puzzle?wordCount=${wordCount}`)
+    
+    if(response.status === 200){
+        const data= await response.json()
+        return data.puzzle
+    }else {
+        throw new Error(`Unable to fetch puzzle`)
+    }
+}
 
-    request.addEventListener('readystatechange',(e) => {
-        if(e.target.readyState === 4 && e.target.status === 200){
-            const data = JSON.parse(e.target.responseText)
-            resolve(data.puzzle)
-        } else if(e.target.readyState === 4){
-            reject(`An error has occured`)
-        }
-    })
+const getCountry = async (countryCode) => {
+    const response = await fetch(`http://restcountries.eu/rest/v2/all`)
+    
+    if(response.status === 200){
+        let data = await response.json()
+        return data.find((country)=> country.alpha2Code === countryCode)
+    } else {
+        throw new Error(`Unable to fetch country`)
+    }
+}
+    
 
-    request.open('GET',`http://puzzle.mead.io/puzzle?wordCount=${wordCount}`)
-    request.send()
-})
 
-const getCountry = (countryCode) =>  new Promise ((resolve, reject) => {
-    // const countryCode = "MX"
-    const countryReq = new XMLHttpRequest()
+const getLocation = async () => {
+    const response = await fetch(`http://ipinfo.io/json?token=f4d2fc5f44c3d8`)
+    
+    if(response.status === 200){
+        return response.json()
+    } else {
+        throw new Error(`Unable to fetch your location`)
+    }
+    
+}
 
-    countryReq.addEventListener('readystatechange', (e) => {
-        if (e.target.readyState === 4 && e.target.status === 200){
-            const data= JSON.parse(e.target.responseText)
-            const country = data.find((country) => country.alpha2Code === countryCode)
-            resolve(country)
-        } else if(e.target.readyState === 4){
-            reject('Unable to fetch data')
-        }
-    })
-
-    countryReq.open('GET','http://restcountries.eu/rest/v2/all')
-    countryReq.send()
-})
+const getCurrentCountry = async() => {
+    let location = await getLocation()
+    const country = await getCountry(location.country)
+    return country     
+} 
